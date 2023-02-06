dbb7d57038bfd6f225f7814a0107aa3dc227fa83
amanda-vlopes <amandavitor@hotmail.com>
2023-02-06 16:17:28 -0300

    Requisito 4 finalizado

--- script.js
+++ script.js
@@ -1,61 +1,45 @@
 // Requisito 2
 // Adicionando cor de fundo de cada elemento da paleta com o for
 
-let cor0 = document.getElementsByClassName('color')[0];
+const cor0 = document.getElementsByClassName('color')[0];
 cor0.style.backgroundColor = 'black';
 
-let cor1 = document.getElementsByClassName('color')[1];
+const cor1 = document.getElementsByClassName('color')[1];
 cor1.style.backgroundColor = 'rgb(0,191,255)';
 
-let cor2 = document.getElementsByClassName('color')[2];
+const cor2 = document.getElementsByClassName('color')[2];
 cor2.style.backgroundColor = 'rgb(127,255,212)';
 
-let cor3 = document.getElementsByClassName('color')[3];
+const cor3 = document.getElementsByClassName('color')[3];
 cor3.style.backgroundColor = 'rgb(255,105,180)';
 
-// criando a função com o for
+// Requisito 3
 
-// Primeira opção pensada porém com complexidade maior que 6
+// Recuperando botão
+const buttonRandonColor = document.getElementById('button-random-color');
 
-// const coresPaleta = () => {
-//   for (let index = 0; index < paletaDeCores.length; index += 1) {
-//     switch (index) {
-//     case 0:
-//       paletaDeCores[0].style.backgroundColor = 'black';
-//       break;
-//     case 1:
-//       paletaDeCores[1].style.backgroundColor = 'rgb(0,191,255)';
-//       break;
-//     case 2:
-//       paletaDeCores[2].style.backgroundColor = 'rgb(127,255,212)';
-//       break;
-//     case 3:
-//       paletaDeCores[3].style.backgroundColor = 'rgb(255,105,180)';
-//       break;
-//     }
-//   }
-// };
-// coresPaleta();
-
-// recuperando o elemento
+// recuperando o elemento de cores
 const paletaDeCores = document.getElementsByClassName('color');
 
 // Gerando cor aleatoria:
+const gerandoCor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+};
 
-// const gerandoCor = () => {
-//   const r = Math.floor(Math.random() * 256);
-//   const g = Math.floor(Math.random() * 256);
-//   const b = Math.floor(Math.random() * 256);
-//   return `rgb(${r},${g},${b})`;
-// };
+// Adicionando função:
 
-// const corPaleta = () => {
-//   for (let index = 0; index < paletaDeCores.length; index += 1) {
-//     if (index === 0) {
-//       paletaDeCores[0].style.backgroundColor = ' black';
-//     } else {
-//       paletaDeCores[index].style.backgroundColor = gerandoCor();
-//     }
-//   }
-// };
-// corPaleta();
+const corPaleta = () => {
+  buttonRandonColor.addEventListener('click', () => {
+    for (let index = 0; index < paletaDeCores.length; index += 1) {
+      if (index === 0) {
+        paletaDeCores[0].style.backgroundColor = ' black';
+      } else {
+        paletaDeCores[index].style.backgroundColor = gerandoCor();
+      }
+    }
+  });
+};
+corPaleta();
