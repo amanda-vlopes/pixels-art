74390878e8dc9485224d19238e83b856ab6f8622
amanda-vlopes <amandavitor@hotmail.com>
2023-02-07 12:41:20 -0300

    Requisito 5 finalizado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Requisito 2
+// Requisito 2 - Adicione à página uma paleta contendo quatro cores distintas.
 // Adicionando cor de fundo de cada elemento da paleta com o for
 
 const cor0 = document.getElementsByClassName('color')[0];
@@ -13,7 +13,7 @@ cor2.style.backgroundColor = 'rgb(127,255,212)';
 const cor3 = document.getElementsByClassName('color')[3];
 cor3.style.backgroundColor = 'rgb(255,105,180)';
 
-// Requisito 4
+// Requisito 4 - Adicione um botão para gerar cores aleatórias para a paleta de cores.
 
 // Recuperando botão
 const buttonRandonColor = document.getElementById('button-random-color');
@@ -29,40 +29,7 @@ const gerandoCor = () => {
   return `rgb(${r},${g},${b})`;
 };
 
-// Adicionando função de armazenamento:
-
-const armazenandoCores = (cores) => {
-  localStorage.setItem('colorPalette', JSON.stringify(cores));
-  JSON.parse(localStorage.getItem('colorPalette'));
-};
-
-// const mantendoCores = () => {
-//   if (localStorage !== null) {
-
-//   }
-// }
-// JSON.parse(localStorage.getItem('colorPalette'))
-
-// Adicionando função:
-
-const coresAleatorias = [];
-
-const corPaleta = () => {
-  buttonRandonColor.addEventListener('click', () => {
-    for (let index = 0; index < paletaDeCores.length; index += 1) {
-      if (index === 0) {
-        paletaDeCores[0].style.backgroundColor = 'rgb(0,0,0)';
-      } else {
-        paletaDeCores[index].style.backgroundColor = gerandoCor();
-      }
-      coresAleatorias.push(paletaDeCores[index].style.backgroundColor);
-    }
-    armazenandoCores(coresAleatorias);
-  });
-};
-corPaleta();
-
-// Opção sem adicionar função por fora
+// Função requisito 4 sem armazenar as cores no localStorage
 
 // const corPaleta = () => {
 //   buttonRandonColor.addEventListener('click', () => {
@@ -72,14 +39,42 @@ corPaleta();
 //       } else {
 //         paletaDeCores[index].style.backgroundColor = gerandoCor();
 //       }
-//       coresAleatorias.push(paletaDeCores[index].style.backgroundColor);
 //     }
-//     localStorage.setItem('colorPalette', JSON.stringify(coresAleatorias));
-//     const armazenandoCores = JSON.parse(localStorage.getItem('colorPalette'));
-//     console.log(armazenandoCores);
 //   });
 // };
 // corPaleta();
+
+// Requisito 5 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página. - Função do requisito 4 usada para armazenar as cores geradas.
+
+const corPaleta = () => {
+  buttonRandonColor.addEventListener('click', () => {
+    const coresAleatorias = [];
+    for (let index = 0; index < paletaDeCores.length; index += 1) {
+      if (index === 0) {
+        paletaDeCores[0].style.backgroundColor = 'rgb(0,0,0)';
+      } else {
+        paletaDeCores[index].style.backgroundColor = gerandoCor();
+      }
+      // Armazenando as cores em um array
+      coresAleatorias.push(paletaDeCores[index].style.backgroundColor);
+    }
+    // Armazenando as cores armazenadas no meu localStorage
+    localStorage.setItem('colorPalette', JSON.stringify(coresAleatorias));
+  });
+};
+corPaleta();
+
+// Função para atribuir as cores de fundo da minha paleta como as cores armazenadas no localStorage
+
+const recarregaCores = () => {
+  const armazenandoCores = JSON.parse(localStorage.getItem('colorPalette'));
+  if (armazenandoCores !== null) {
+    for (let index = 0; index < paletaDeCores.length; index += 1) {
+      paletaDeCores[index].style.backgroundColor = armazenandoCores[index];
+    }
+  }
+};
+recarregaCores();
 
 // Requisito 6
 
